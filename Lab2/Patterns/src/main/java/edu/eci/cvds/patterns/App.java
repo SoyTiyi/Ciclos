d1863c3471dfc4ac1fc1d87431651bed82383fef
SoyTiyi <santiago.marmartinez@gmail.com>
2020-08-13 08:56:48 -0500

    add last name

--- Lab2/Patterns/src/main/java/edu/eci/cvds/patterns/App.java
+++ Lab2/Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -11,7 +11,11 @@ public class App
     {
         System.out.println(args.length+"Perfil");
 	    if(args.length>0){
-			System.out.println("Hello "+args[0]+"!");
+		String resp="";
+	    	for(int i=0;i<args.length;i++){
+			resp+=args.length[i]+" ";
+		}
+		System.out.println("Hello "+resp+"!");
 	    } else {
 			System.out.println("Hello World!");
 	    }
--- README.md
+++
@@ -1,2 +0,0 @@
-# Ciclos
-CVDS-1
